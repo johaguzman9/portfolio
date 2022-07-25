8adc42be7da34c29a240078e48f389f6837c8a2b
johaguzman9 <jandreinagrivas@hotmail.com>
2022-07-25 20:32:46 -0300

    cambios

---
+++ confirmacion-envio.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmacion</title>
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<main class="container">
+<body id="contacto" class="d-flex flex-column h-100">
+    <header class="container">
+    <?php include_once "menu.php";?>
+    <div class="col-12 text-center col-sm-6">
+      <p>Gracias por contactarse nos comunicaremos a la brevedad</p>
+    </div>
+    </header>
+</main>
+
+<footer class="container mt-auto pb-4">
+        <div class="btn-whatsapp">
+            <i class="fa-brands fa-whatsapp"></i>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <a href="" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+                <a href="" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-3">
+                Sponsor <a href="https://depcsuite.com">Depcsuite</a>
+            </div>
+            <div class="col-3">
+                <a href="mailto:johaandreinagrivas@gmail.com">johaandreina09@gmail.com</a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
--- contacto.php
+++ contacto.php
@@ -1,6 +1,37 @@
 <?php
     $pg = "contacto";
-?>
+    if($_POST){
+        $nombre = $_POST["txtNombre"];
+        $correo = $_POST["txtCorreo"];
+        $telefono = $_POST["txtTelefono"];
+        $mensaje = $_POST["txtMensaje"];
+    
+        // Varios destinatarios
+        $para = "johaandreina09@gmail.com";
+        $titulo = 'Recibiste un mensaje desde tu Web';
+    
+        // mensaje
+        $cuerpo = "
+        Nombre: $nombre <br>
+        Correo: $correo <br>
+        Telefono: $telefono <br>
+        Mensaje: $mensaje
+        ";
+    
+        // Para enviar un correo HTML, debe establecerse la cabecera Content-type
+        $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+        $cabeceras .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+    
+        // Cabeceras adicionales
+        $cabeceras .= 'To: johaandreina09@gmail.com' . "\r\n";
+        $cabeceras .= 'From: contacto@johannysguzman.com.ar' . "\r\n";
+    
+        // Enviarlo
+        //mail($para, $titulo, $cuerpo, $cabeceras);
+        header("Location: confirmacion-envio.php");
+    }
+    ?>
+
 
 
 <!DOCTYPE html>
